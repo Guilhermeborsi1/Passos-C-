f66b52af32036918ce1ac1288c5091186a9e2724
Guilherme_Borsi <106710661+Guilhermeborsi1@users.noreply.github.com>
2022-08-10 22:30:04 -0300

    Passos C#

---
+++ Passos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Corrida_animais.Objetos
+{
+    class Passos
+    {
+        public void andar(int n_passos)
+        {
+            for (int i = 0; i < n_passos; i++)
+            {
+                Console.WriteLine("passos n" + i.ToString());
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Corrida_animais.Objetos;
+
+namespace Corrida_animais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Passos oPasso = new Passos();
+            
+            Rotulo_animal:
+            Console.WriteLine("Digite o animal gato, cachorro ou cavalo: ");
+            string animal = Console.ReadLine();
+
+            if(animal.ToUpper() == "GATO" || animal.ToUpper() == "CACHORRO" || animal.ToUpper() == "CAVALO" )
+                {}
+            else 
+            {
+                Console.WriteLine("Digite corretamente");
+                Console.WriteLine("Cachorro, Gato ou Cavalo");
+                goto Rotulo_animal;
+            }
+
+            Rotulo_passos:
+            Console.WriteLine("Digite os passos necessarios: ");
+            string passos = Console.ReadLine();
+
+            if (passos == "1" || 
+                passos == "2" || 
+                passos == "3" || 
+                passos == "4" || 
+                passos == "5" || 
+                passos == "6")
+            {
+                oPasso.andar(int.Parse(passos));
+            }
+            else
+            {
+                Console.WriteLine("Digite corretamente");
+                Console.WriteLine("De 1 a 6");
+                goto Rotulo_passos;
+
+                
+
+            }
+        }
+    }
+}
